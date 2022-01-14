b3cd68ea8000f2771f0fa02eb8d44d6d448521bf
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-01-14 13:12:50 -0300

    Unzip Example ReactNative

---
+++ Exemplos/App_eXperience_ReactNative/E1_AppExperience_ReactNative-font/elginm8-main/src/pages/Printer.js
@@ -0,0 +1,364 @@
+import React, { useState, useEffect }from 'react';
+import Footer from '../components/Footer'
+import RadioForm from 'react-native-simple-radio-button';
+
+import { RadioButton } from 'react-native-paper';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+    TextInput,
+    DeviceEventEmitter,
+    Alert
+} from 'react-native';
+   
+import Header from '../components/Header';
+import PrinterBarCode from './PrinterOptions/PrinterBarCode';
+import PrinterImage from './PrinterOptions/PrinterImage';
+import PrinterText from './PrinterOptions/PrinterText';
+
+import PrinterService from '../services/service_printer';
+
+const Printer =()=> {
+    var printerService = new PrinterService();
+
+    const [typePrinter,setTypePrinter]=useState('TEXT');
+
+    const [checked, setChecked] = useState('M8');
+
+    const [ipConection,setIpConection]=useState('192.168.0.31:9100');
+    const [ isUsingPrinterExtern, setIsUsingPrinterExtern ] = useState(false);
+    
+    const buttonsPrinter = [
+        {id:'TEXT', icon: require('../icons/printerText.png'), textButton: 'IMPRESSÃO\nDE TEXTO',onPress:()=> setTypePrinter('TEXT')},
+        {id:'BARCODE', icon: require('../icons/printerBarCode.png'), textButton: 'IMPRESSÃO DE\nCÓDIGO DE BARRAS',onPress:()=>setTypePrinter('BARCODE')},
+        {id:'IMAGE', icon: require('../icons/printerImage.png'), textButton: 'IMPRESSÃO\nDE IMAGEM',onPress:()=>setTypePrinter('IMAGE')},
+    ];
+    
+    useEffect(() => {
+        startConnectPrinterIntern();
+    },[]);
+
+    const typePrinterAtual=()=>{
+        switch(typePrinter){
+            case 'TEXT':
+                return(<PrinterText/>)
+            case 'BARCODE':
+                return(<PrinterBarCode/>)
+            case 'IMAGE':
+                return(<PrinterImage/>)
+        }
+    };
+
+    function actualStatusPrinter(){
+        printerService.getStatusPrinter();
+
+        let actualEvent = DeviceEventEmitter.addListener('eventStatusPrinter', event => {
+            var actualReturn = event.statusPrinter;
+
+            if(actualReturn === '5'){
+                Alert.alert("Retorno", "Papel está presente e não está próximo!");
+            }else if(actualReturn === '6'){
+                Alert.alert("Retorno", "Papel está próximo do fim!");
+            }else if(actualReturn === '7'){
+                Alert.alert("Retorno", "Papel ausente!");
+            }else{
+                Alert.alert("Retorno", "Status Desconhecido");
+            }
+        });
+
+        setTimeout(() => {
+            actualEvent.remove();
+        }, 2000)        
+    };
+
+    function actualStatusGaveta(){
+        printerService.getStatusGaveta();
+
+        let actualEvent = DeviceEventEmitter.addListener('eventStatusGaveta', event => {
+            var actualReturn = event.statusGaveta;
+
+            if(actualReturn === '1'){
+                Alert.alert("Retorno", "Gaveta aberta!");
+            }else if(actualReturn === '2'){
+                Alert.alert("Retorno", "Gaveta fechada!");
+            }else{
+                Alert.alert("Retorno", "Status Desconhecido");
+            }
+        });
+
+        setTimeout(() => {
+            actualEvent.remove();
+        }, 2000)        
+    };
+
+    function sendAbrirGaveta(){
+        printerService.sendOpenGaveta();
+    }
+
+    function changePrinterChoose(value){
+        if(value === "I9"){
+            setIsUsingPrinterExtern(true);
+            startConnectPrinterExtern();
+        }else{
+            setIsUsingPrinterExtern(false);
+            startConnectPrinterIntern();
+        }        
+    };
+
+    function startConnectPrinterIntern(){
+        setChecked("M8");
+        printerService.sendStartConnectionPrinterIntern();
+    };
+
+    function startConnectPrinterExtern(){
+        if(ipConection !== ''){
+            var ip = ipConection.split(":")[0];
+            var port = ipConection.split(":")[1];
+
+            if(isIpAdressValid()){
+                printerService.sendStartConnectionPrinterExtern(
+                    ip,
+                    parseInt(port),
+                );
+                setChecked("I9");
+            }else{
+                Alert.alert("Alert", "Digíte um endereço e porta IP válido!");
+            }   
+        }else{
+            Alert.alert("Alert", "Digíte um endereço e porta IP válido!");
+        }        
+    };
+
+    function isIpAdressValid(){
+        let ipValid = false;
+
+        if((/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\:(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?[0-9][0-9])$/.test(ipConection))){
+            ipValid = true;
+            return ipValid;
+        }else{
+            ipValid = false;
+            return ipValid;
+        };
+    };
+
+    return(
+        <View style={styles.mainView}>
+            <Header textTitle='IMPRESSORA'/>
+            <View style={styles.menuView}>
+                <View style={styles.optonsMenuView}>
+                    {buttonsPrinter.map(({id,icon,textButton,onPress}, index)=>(
+                        <TouchableOpacity 
+                            style={[styles.buttonMenu, {borderColor: id===typePrinter ? '#0069A5':'black'}]} 
+                            key={index}
+                            onPress={onPress}
+                        >
+                                <Image style={styles.icon} source={icon}/>
+                                <Text style={styles.menuTextButton}>{textButton}</Text>
+                        </TouchableOpacity>
+                    ))}
+                    <TouchableOpacity style={styles.statusButton} onPress={actualStatusPrinter}>
+                        <Image style={styles.statusIcon} source={require('../icons/status.png')}/>
+                        <Text style={styles.statusButtonTXT}>STATUS IMPRESSORA</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.statusButton} onPress={actualStatusGaveta}>
+                        <Image style={styles.statusIcon} source={require('../icons/status.png')}/>
+                        <Text style={styles.statusButtonTXT}>STATUS GAVETA</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.actionButton} onPress={sendAbrirGaveta}>
+                        <Text style={styles.actionButtonTXT}>ABRIR GAVETA</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.settingsPrinterView}>
+                    <View style={styles.settingsPrinterHeader}>
+                        <View style={styles.typePrinterOptionView}>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>                            
+                            <RadioButton
+                                value="M8"
+                                color="#0069A5"
+                                status={ checked === 'M8' ? 'checked' : 'unchecked' }
+                                onPress={() => changePrinterChoose('M8')}
+                            />
+                            <Text style={styles.labelText}>IMP. INTERNA</Text>
+                        </View>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>                            
+                            <RadioButton
+                                value="I9"
+                                color="#0069A5"
+                                status={ checked === 'I9' ? 'checked' : 'unchecked' }
+                                onPress={() => changePrinterChoose('I9')}
+                            />
+                            <Text style={styles.labelText}>IMP. EXTERNA</Text>
+                        </View>                                               
+                        </View>
+                        <View style={styles.conectionView}>
+                            <Text style={styles.textSizeDefault}>IP:</Text>
+                            <TextInput                                                                
+                                style={styles.inputMensage}
+                                placeholder="192.168.0.1:9100"
+                                placeholderTextColor="#999"
+                                autoCapitalize="none"
+                                keyboardType='default'
+                                autoCorrect={false}
+                                onChangeText={setIpConection}
+                                value={ipConection}
+                            />
+                        </View>
+                        
+                    </View>
+                    <View style={styles.settingPrinterBody}>
+                        {typePrinterAtual()}
+                    </View>
+                </View>
+            </View>
+            <Footer />
+        </View>
+    );
+ };
+
+ const styles =StyleSheet.create({
+    mainView:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'space-between',
+        backgroundColor:'white',
+    },
+    menuView:{
+        flexDirection:'row',
+        width:'90%',
+        height:400,
+        justifyContent:'space-between',
+    },
+    optonsMenuView:{
+        flexDirection:'column',
+        height:'100%',
+        alignItems:'center',
+        justifyContent:"space-around",
+    },
+    buttonMenu:{
+        borderWidth:2,
+        borderColor:'black',
+        width:150,
+        height:80,
+        fontWeight:'bold',
+        borderRadius:10,
+        justifyContent:'center',
+        alignItems:'center',
+        marginBottom:8,
+    },
+    actionButton:{
+        width:'100%',
+        height:35,
+        backgroundColor:'#0069A5',
+        alignItems:'center',
+        borderRadius:5,
+        justifyContent:'center',
+    },
+    actionButtonTXT:{
+        color:'white',
+        fontWeight:'bold',
+    },
+    icon:{
+        width:50,
+        height:50,
+    },
+    labelText:{
+        fontWeight:'bold',
+        fontSize:15,
+    },
+    menuTextButton:{
+        textAlign:'center',
+        fontSize:10,
+        fontWeight: 'bold'
+    },
+    textButton:{
+        color:'white',
+        fontWeight:'bold'
+    },
+    statusButton:{
+        flexDirection:'row',
+        width:150,
+        height:50,
+        borderWidth:2,
+        borderRadius:5,
+        borderColor:'black',
+        alignItems:'center',
+        marginBottom:7,
+       
+    },
+    statusButtonTXT:{
+        fontSize:10,
+        marginLeft:5,
+    },
+    statusIcon:{
+        width:20,
+        height:20,
+        marginLeft:5,
+    },
+    settingsPrinterView:{
+        width:'78%',
+        height:'100%',
+        flexDirection:'column',    
+    },
+    settingsPrinterHeader:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        height:'10%',
+        paddingBottom:5,
+    },
+    typePrinterOptionView:{
+        flexDirection:'row',
+        alignContent:'center',
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    settingPrinterBody:{
+        width:'100%',
+        height:'90%',
+        borderColor:'black',
+        borderWidth:2,
+        borderRadius:5,
+        alignItems:'center',
+        justifyContent:'space-between'
+    },
+    conectionView:{
+        flexDirection:'row',
+        paddingLeft:5,
+        borderWidth:2,
+        borderRadius:15,
+        borderColor:'black',
+        justifyContent:'center',
+        width: 210,
+        height:'100%',
+        alignItems:'center',
+        alignContent:'center'
+    },
+    conectionIpText:{
+        fontWeight:'bold',
+    },
+    conectionButton:{
+        backgroundColor:'#0069A5',
+        borderWidth:2,
+        borderRadius:15,
+        borderColor:'#0069A5',
+        height:'100%',
+        alignItems:'center',
+        alignContent:'center',
+        justifyContent:'center',
+        width:150
+    },
+    inputMensage: {
+        width: '80%',
+        // borderBottomColor: 'black',
+        // borderBottomWidth: 2,
+        fontSize: 16,
+        color: 'black',
+        // textAlignVertical: 'bottom',
+        padding: 0
+    },
+ });
+
+ export default Printer;
---
+++ Exemplos/App_eXperience_ReactNative/E1_AppExperience_ReactNative-font/elginm8-main/src/pages/SAT.js
@@ -0,0 +1,367 @@
+import React, { useState } from 'react';
+import RadioForm from 'react-native-simple-radio-button';
+
+import {
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    ScrollView,
+    DeviceEventEmitter,
+  } from 'react-native';
+
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+import ServiceSat from '../services/service_sat';
+
+import XmlEnviarDadosVendaSat from '../services/service_xmlEnviarDadosVendaSat';
+import XmlCancelarUltimaVendaSat from '../services/service_xmlCancelarUltimaVendaSat';
+import XmlSatGo from '../services/service_xmlSatGo';
+
+import { TextInput } from 'react-native-gesture-handler';
+
+const satOptionsRadioButton = [
+    {
+        label: 'SMART SAT',
+        value: 'SMART SAT'
+    },
+    {
+        label: 'SATGO',
+        value: 'SATGO'
+    },
+  
+];
+
+
+const SAT = () => {
+
+    var xmlEnviarDadosVendaSat = new XmlEnviarDadosVendaSat();
+    var xmlCancelarUltimaVendaSat = new XmlCancelarUltimaVendaSat();
+    var xmlSATGo =  new XmlSatGo();
+
+    var satService = new ServiceSat();
+
+    const [cfeCancelamento, setCFeCancelamento] = useState("");
+    const [textReturn,setTextReturn] = useState("");
+    const [selectedOptionSat,setSelectedOptionSat] =useState("SMART SAT");
+    const [activationCode,setActvationCode] = useState("123456789");
+    
+
+    const satButtonsLeft = [
+        { textButton: 'CONSULTAR SAT', onPress: () => sendConsultarSat()},
+        { textButton: 'STATUS OPERACIONAL', onPress: () => sendStatusOperacional()},
+        { textButton: 'REALIZAR VENDA', onPress: () => enviarDadosVendaSat()},          
+    ]
+
+    const satButtonsRigth = [
+        { textButton: 'CANCELAMENTO', onPress: () => cancelarVendaSat()},
+        { textButton: 'ATIVAR', onPress: () => sendAtivarSat()},
+        { textButton: 'ASSOCIAR', onPress: () => sendAssociarSat()},          
+    ]
+
+    function sendAtivarSat() {
+        var numSessao = Math.floor(Math.random() * 999999).toString();
+    
+        satService.sendActivateSat(
+            parseInt(numSessao),
+            2,
+            activationCode,
+            "14200166000166",
+            15,
+        );
+        
+        let actualEvent = DeviceEventEmitter.addListener('eventAtivarSat', event => {
+            var actualReturn = event.resultAtivarSat;
+            setTextReturn(actualReturn);
+        })
+
+        setTimeout(() => {
+            actualEvent.remove();
+        }, 2000)     
+    }
+
+    function sendAssociarSat(){
+        var numSessao = Math.floor(Math.random() * 999999).toString();
+
+        satService.sendAssociateSignature(
+            parseInt(numSessao),
+            activationCode,
+            "16716114000172",
+            "SGR-SAT SISTEMA DE GESTAO E RETAGUARDA DO SAT"
+        );
+
+        let actualEvent = DeviceEventEmitter.addListener('eventAssociateSignature',event=> {
+            var actualReturn = event.resultAssociateSignature;
+            setTextReturn(actualReturn);
+        })
+
+        setTimeout(() => {
+            actualEvent.remove();
+        }, 2000)     
+    }
+
+    function sendConsultarSat(){
+        var numSessao = Math.floor(Math.random() * 999999).toString();
+
+        const mapParam={
+            "typeSat":"consultSat",
+            "numSessao": parseInt(numSessao),
+        }
+
+        satService.sendFunctionToAndroid(mapParam)
+
+        let actualEvent = DeviceEventEmitter.addListener('eventConsultSat',event=> {
+            var actualReturn = event.resultConsultSat;
+            setTextReturn(actualReturn);
+        })
+
+        setTimeout(() => {
+            actualEvent.remove();
+        }, 2000)     
+       
+    }
+
+    function sendStatusOperacional(){
+        var numSessao = Math.floor(Math.random() * 999999).toString();
+
+        satService.sendStatusSat(
+            parseInt(numSessao),
+            activationCode,
+        )
+        let actualEvent = DeviceEventEmitter.addListener('eventStatusOperacional',event=> {
+            var actualReturn = event.resultStatusOperacional;
+            setTextReturn(actualReturn);
+        })
+
+        setTimeout(() => {
+            actualEvent.remove();
+        }, 2000)     
+  
+    }
+
+    function enviarDadosVendaSat(){
+        var numSessao = Math.floor(Math.random() * 999999).toString();
+
+        var xmlSale = "";
+        setCFeCancelamento("");
+
+        if(selectedOptionSat === "SMART SAT"){
+            xmlSale = xmlEnviarDadosVendaSat.getXmlEnviarDadosVendaSat();
+        }else{
+            xmlSale = xmlSATGo.getXmlSatGo();
+        }
+
+        satService.sendSendSell(
+            parseInt(numSessao),
+            activationCode,
+            xmlSale
+        )
+
+        let actualEvent = DeviceEventEmitter.addListener('eventSendSell',event=> {
+            var actualReturn = event.resultSendSell;
+
+            //TRATAMENTO PARA PEGAR O CÓDIGO CFE ATUAL PARA CANCELAMENTO
+            if(actualReturn.includes('|')){
+                var listOfReturn = actualReturn.split('|');
+                var newReturn = listOfReturn.find(value => value.includes('CFe'))
+                if(newReturn){
+                    setCFeCancelamento(newReturn);
+                };
+                
+            }
+            setTextReturn(actualReturn);
+        })
+
+        setTimeout(() => {
+            actualEvent.remove();
+        }, 2000)
+    }
+
+    function cancelarVendaSat(){
+        var numSessao = Math.floor(Math.random() * 999999).toString();
+        const xmlCancel = xmlCancelarUltimaVendaSat.getXmlCancelarUltimaVendaSat();
+
+        var newXmlCancel = xmlCancel.replace('novoCFe', cfeCancelamento);
+
+        satService.sendCancelSell(
+            parseInt(numSessao),
+            activationCode,
+            cfeCancelamento.toString(),
+            newXmlCancel,
+        )
+
+        let actualEvent = DeviceEventEmitter.addListener('eventCancelSell',event=> {
+            var actualReturn = event.resultCancelSell;
+            setTextReturn(actualReturn);
+        })
+
+        setTimeout(() => {
+            actualEvent.remove();
+        }, 2000)     
+    }
+
+    return(
+        <View style={styles.mainView} >
+            <Header textTitle={'SAT'}/>
+            <View style={styles.satMenuView}>
+                <View style={styles.returnSatView}>
+                    <Text style={styles.labelText}>RETORNO SAT</Text>
+                    <ScrollView>
+                        <Text style={styles.returnText}>
+                            {textReturn}
+                        </Text>                    
+                    </ScrollView>                    
+                </View>
+                <View style={styles.inputSatView}>
+                    <View style={styles.radioButtonsView}>
+                        <RadioForm
+                            style={{marginVertical:10}}
+                            labelStyle={{marginHorizontal:5}}
+                            radio_props={satOptionsRadioButton}
+                            initial={0}
+                            buttonSize={10}
+                            formHorizontal={true}
+                            onPress={(item) => setSelectedOptionSat(item)}
+                        />
+                    </View>
+                    <View style={styles.inputCodeView}>
+                        <Text style={styles.optionText}>Código de Ativação: </Text>
+                        <TextInput
+                            placeholder={'000'}
+                            style={styles.inputMensage}
+                            keyboardType='numeric'
+                            onChangeText={setActvationCode}
+                            value={activationCode}              
+                        />            
+                    </View>
+                   <View style={styles.actionButtonsView}>
+                       <View style={styles.buttonsView}>
+                           {satButtonsLeft.map(({id,textButton,onPress},index)=>(
+                                <TouchableOpacity 
+                                    style={styles.submitionButton} 
+                                    key={index}
+                                    onPress={onPress}
+                                >
+                                        <Text style={styles.textButton}>{textButton}</Text>
+                                </TouchableOpacity>
+                            ))}
+                       </View>
+                       <View style={styles.buttonsView}>
+                           {satButtonsRigth.map(({id,textButton,onPress},index)=>(
+                                <TouchableOpacity 
+                                    style={styles.submitionButton} 
+                                    key={index}
+                                    onPress={onPress}
+                                >
+                                        <Text style={styles.textButton}>{textButton}</Text>
+                                </TouchableOpacity>
+                            ))}
+                       </View>
+                   </View>
+                </View>
+            </View>            
+            <Footer/>
+        </View>
+    );
+
+};
+
+const styles=StyleSheet.create({
+    mainView:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'space-between',
+        backgroundColor:'white',
+
+    },
+    labelText:{
+        fontWeight:'bold',
+        fontSize:14,
+        marginBottom:20,
+    },
+    optionText:{
+        fontSize:14,
+        fontWeight:'bold',
+    },
+    titleText:{
+        textAlign:'center',
+        fontSize:30,
+        fontWeight:'bold',
+    },
+    satMenuView:{
+        flexDirection:'row',
+        height:450,
+        width:'90%',
+        justifyContent:'space-between'
+    },
+    returnSatView:{
+        flexDirection:'column',
+        width:'49%',
+        padding:30,
+        borderWidth:2.5,
+        borderColor:'black',
+        borderRadius:10,
+    },
+    inputSatView:{
+        flexDirection:'column',
+        width:'49%',
+        alignItems:'center',
+        justifyContent:'center',
+        
+    },
+    radioButtonsView:{
+        
+      
+        width:'100%',
+    },
+    inputCodeView:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        width:'100%',
+        marginVertical:25,
+        alignContent:'center',
+        alignItems:'center',
+    },
+    inputMensage:{
+        borderBottomWidth:1,
+        borderBottomColor:'black',
+        width:"50%",
+     
+    },
+    actionButtonsView:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        width:'100%',
+     
+    },
+    buttonsView:{
+        width:'48%',
+        alignItems:'center',
+       
+    },
+    submitionButton:{
+        width:'100%',
+        height:40,
+        backgroundColor:'#0069A5',
+        alignItems:'center',
+        borderRadius:13,
+        justifyContent:'center',
+        margin:5,
+    },
+    textButton:{
+        color:'white',
+        fontWeight:'bold',
+        fontSize:14,
+    },
+    returnText:{
+        borderBottomWidth:2,
+        borderBottomColor:'black',
+        paddingBottom:30,
+        marginBottom:20,
+    }
+});
+
+
+
+export default SAT;
