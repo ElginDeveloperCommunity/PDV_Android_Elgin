99f0b95da530c7130043c15dadf529096ada7dd0
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-03-25 18:12:59 -0300

    Update Example Java M10

--- Exemplos/App_eXperience_Java/Elgin_AppExperience_Java-font/app/src/main/java/com/elgin/elginexperience/SatPage.java
+++ Exemplos/App_eXperience_Java/Elgin_AppExperience_Java-font/app/src/main/java/com/elgin/elginexperience/SatPage.java
@@ -1,16 +1,23 @@
 package com.elgin.elginexperience;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.elgin.elginexperience.Services.SatService;
 
@@ -23,6 +30,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
+
+import br.com.elgin.Sat;
 
 public class SatPage extends AppCompatActivity {
     static Context context;
@@ -40,6 +49,7 @@ public class SatPage extends AppCompatActivity {
     Button buttonCancelamentoSAT;
     Button buttonAtivarSAT;
     Button buttonAssociarSAT;
+    Button buttonExtrairLogSat;
 
     String xmlEnviaDadosVenda = "xmlenviadadosvendasat";
     String xmlCancelamento = "cancelamentosatgo";
@@ -47,12 +57,18 @@ public class SatPage extends AppCompatActivity {
     String cfeCancelamento = "";
     String typeModelSAT = "SMART SAT";
 
+    public String logSavedMessage;
+
+    private static final int REQUEST_CODE_WRITE_EXTERNAL_STORAGE = 1234;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sat_page);
         context = this;
         serviceSat = new SatService(context);
+
+        logSavedMessage = "Log Sat Salvo em " + serviceSat.getBASE_ROOT_DIR();
 
         textRetorno = findViewById(R.id.textRetorno);
 
@@ -66,6 +82,7 @@ public class SatPage extends AppCompatActivity {
         buttonCancelamentoSAT = findViewById(R.id.buttonCancelamentoSAT);
         buttonAtivarSAT = findViewById(R.id.buttonAtivarSAT);
         buttonAssociarSAT = findViewById(R.id.buttonAssociarSAT);
+        buttonExtrairLogSat = findViewById(R.id.buttonExtrairLogSat);
 
         //CONFIGS MODEL BALANÇA
         radioButtonSMARTSAT.setChecked(true);
@@ -124,6 +141,29 @@ public class SatPage extends AppCompatActivity {
         buttonAssociarSAT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) { sendAssociarSAT(); }
+        });
+
+        buttonExtrairLogSat.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+               //Checa se a permissão necessária esta garantida e se não, a mesma deve ser pedida para continuar com a função
+               if(isStoragePermissionGranted()){
+                   Map<String, Object> mapValues = new HashMap<>();
+
+                   mapValues.put("numSessao", getNumeroSessao());
+                   mapValues.put("codeAtivacao", editTextInputCodeAtivacao.getText().toString());
+
+                   //Verifica se a extração de log foi bem-sucedida ou não
+                   boolean wasExtractedLogSucessful = serviceSat.extrairLog(mapValues);
+
+                   //Se a extração de log foi bem sucedida, exibir a mensagem de salvamento no campo de retorno
+                   if(wasExtractedLogSucessful) textRetorno.setText(logSavedMessage);
+                   //Caso contrário, exiba a mensagem padrão de retorno de Sat não conectado/encontrado
+                   else { textRetorno.setText("DeviceNotFound"); }
+
+               }
+            }
         });
     }
 
@@ -272,4 +312,52 @@ public class SatPage extends AppCompatActivity {
     private int getNumeroSessao() {
         return new Random().nextInt(1_000_000);
     }
+
+    //Checa se a permissão para escrever arquivos em diretórios externos foi garantida, se não tiver ; peça novamente
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                Log.v("DEBUG","A permissão está garantida!");
+                return true;
+            } else {
+                Log.v("DEBUG","A permissão está negada!");
+                //Pedindo permissão
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //A permissão é automaticamente concecida em sdk > 23 na instalação
+            Log.v("DEBUG","A permissão está garantida!");
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == REQUEST_CODE_WRITE_EXTERNAL_STORAGE && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            Log.v("DEBUG","Permission: "+permissions[0]+ "was "+grantResults[0]);
+            //A permissão necessária acabou de ser garantida, continue com a operação em questão
+            Map<String, Object> mapValues = new HashMap<>();
+
+            mapValues.put("numSessao", getNumeroSessao());
+            mapValues.put("codeAtivacao", editTextInputCodeAtivacao.getText().toString());
+
+            serviceSat.extrairLog(mapValues);
+
+            //Verifica se a extração de log foi bem-sucedida ou não
+            boolean wasExtractedLogSucessful = serviceSat.extrairLog(mapValues);
+
+            //Se a extração de log foi bem sucedida, exibir a mensagem de salvamento no campo de retorno
+            if(wasExtractedLogSucessful) textRetorno.setText(logSavedMessage);
+                //Caso contrário, exiba a mensagem padrão de retorno de Sat não conectado/encontrado
+            else { textRetorno.setText("DeviceNotFound"); }
+
+        }else if(requestCode == REQUEST_CODE_WRITE_EXTERNAL_STORAGE){
+            Toast.makeText(context, "O log da SAT é salvo em um arquivo .txt no storage do dispositivo e é necessário garantir a permissão para tal operação!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
 }
