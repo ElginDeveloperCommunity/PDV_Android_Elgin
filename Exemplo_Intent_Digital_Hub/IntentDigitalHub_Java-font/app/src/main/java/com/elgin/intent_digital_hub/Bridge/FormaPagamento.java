bcadb9fe9c3b7bbe429362be71022086f68da44b
Gabriel Franzeri <gabriel.franzeri@elgin.com.br>
2022-06-27 08:50:07 -0300

    corrected idh

--- Exemplo_Intent_Digital_Hub/IntentDigitalHub_Java-font/app/src/main/java/com/elgin/intent_digital_hub/Bridge/FormaPagamento.java
+++
@@ -1,9 +0,0 @@
-package com.elgin.intent_digital_hub.Bridge;
-
-/**
- * Formas de pagamento disponíveis, utilizadas para diferenciação ao executar o envio de uma transação
- */
-
-public enum FormaPagamento {
-    DEBITO, CREDITO;
-}
